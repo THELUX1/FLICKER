11a5bfd32541998571a62cd82376093f85d13007
THELUX1 <122177610+THELUX1@users.noreply.github.com>
2025-02-28 15:33:12 -0300

    Add files via upload

--- videos.js
+++ videos.js
@@ -1,15 +1,52 @@
 // videos.js
 const movieVideos = {
-    "1294203": {
-        "360p": "https://nueva-url-360p.m3u8",  // URL única
-        "720p": "https://nueva-url-720p.m3u8",  // URL única
-        "1080p": "https://nueva-url-1080p.m3u8" // URL única
+"1160956": {
+        "title": "Operación Panda: Misión Rescate",
+        "360p": "https://xyve3dzn2w.guardstorage.net/hls_3/LVE6hxTN6Du0Vv17Ywq_ipz8moyFa42b8i0AN9w3zJPnSTsEDLnRE6U6SW8G4Hgad3MW176UMLPu61kwafWy7Q/index-f1-v1-a1.m3u8?sig=dUFag4cdvDrTMjLLykpvjw&expires=1740765843",
+        "720p": "https://xyve3dzn2w.guardstorage.net/hls_3/LVE6hxTN6Du0Vv17Ywq_ipz8moyFa42b8i0AN9w3zJPnSTsEDLnRE6U6SW8G4Hgad3MW176UMLPu61kwafWy7Q/index-f2-v1-a1.m3u8?sig=dUFag4cdvDrTMjLLykpvjw&expires=1740765843",
+        "1080p": "https://xyve3dzn2w.guardstorage.net/hls_3/LVE6hxTN6Du0Vv17Ywq_ipz8moyFa42b8i0AN9w3zJPnSTsEDLnRE6U6SW8G4Hgad3MW176UMLPu61kwafWy7Q/index-f3-v1-a1.m3u8?sig=dUFag4cdvDrTMjLLykpvjw&expires=1740765843"
+    },
+"822119": {
+        "title": "Capitán América: Un nuevo mundo",
+        "360p": "https://wayyrm3efe.guardstorage.net/hls_3/m7xRpfOTH7U_jpz19n7TKk35mE7HzR-C6mzUyo5N1hyNPMB47VQt-7LvHm9OouHx3XlN7ihWv-A6hke_xyO4gQtRhrA7GTKBMVM7bxrzb0S1oQZpSwdaHndr-7B4TjeyFnVC9a131NiGRF_y_J-4UMzN0MigwCrAgIxo-5kLiRuJMxfhGXmrwgTNsvuxqsPr1Rrv5yUfYX7fw0sJ7I0j_Q/index-f1-v1-a1.m3u8?sig=DUm6AcvWTD1yZjMj-ndv_Q&expires=1740744259",
+        "720p": "https://wayyrm3efe.guardstorage.net/hls_3/m7xRpfOTH7U_jpz19n7TKk35mE7HzR-C6mzUyo5N1hyNPMB47VQt-7LvHm9OouHx3XlN7ihWv-A6hke_xyO4gQtRhrA7GTKBMVM7bxrzb0S1oQZpSwdaHndr-7B4TjeyFnVC9a131NiGRF_y_J-4UMzN0MigwCrAgIxo-5kLiRuJMxfhGXmrwgTNsvuxqsPr1Rrv5yUfYX7fw0sJ7I0j_Q/index-f2-v1-a1.m3u8?sig=DUm6AcvWTD1yZjMj-ndv_Q&expires=1740744259",
+        "1080p": "https://wayyrm3efe.guardstorage.net/hls_3/m7xRpfOTH7U_jpz19n7TKk35mE7HzR-C6mzUyo5N1hyNPMB47VQt-7LvHm9OouHx3XlN7ihWv-A6hke_xyO4gQtRhrA7GTKBMVM7bxrzb0S1oQZpSwdaHndr-7B4TjeyFnVC9a131NiGRF_y_J-4UMzN0MigwCrAgIxo-5kLiRuJMxfhGXmrwgTNsvuxqsPr1Rrv5yUfYX7fw0sJ7I0j_Q/index-f3-v1-a1.m3u8?sig=DUm6AcvWTD1yZjMj-ndv_Q&expires=1740744259"
+    },
+"1084199": {
+        "title": "Compañera Perfecta",
+        "360p": "https://7p1z2bdanj.guardstorage.net/hls_3/54zdM7ch_xRzf5_DxzPXQGVvH1qSaSh7-tsG8fnDMPvBGvtNT6hwHHXq99CfL3X51nQaT0NKUHC4SaCRh18IkQ/index-f2-v1-a1.m3u8?sig=lHgW0INDiGxrNureytJxRg&expires=1740743974",
+        "720p": "https://7p1z2bdanj.guardstorage.net/hls_3/54zdM7ch_xRzf5_DxzPXQGVvH1qSaSh7-tsG8fnDMPvBGvtNT6hwHHXq99CfL3X51nQaT0NKUHC4SaCRh18IkQ/index-f3-v1-a1.m3u8?sig=lHgW0INDiGxrNureytJxRg&expires=1740743974",
+        "1080p": "https://7p1z2bdanj.guardstorage.net/hls_3/54zdM7ch_xRzf5_DxzPXQGVvH1qSaSh7-tsG8fnDMPvBGvtNT6hwHHXq99CfL3X51nQaT0NKUHC4SaCRh18IkQ/index-f3-v1-a1.m3u8?sig=lHgW0INDiGxrNureytJxRg&expires=1740743974"
+    },
+"1241982": {
+        "title": "Moana 2",
+        "360p": "https://0uki2r61lp.guardstorage.net/hls_3/VcJ1yQrvRQTswJn-PEa4fjoSsaG8KCxs-pM4-uFYU7K53VtwMO1mKQLlu7DFkmW47JGe-ytPzDdDfXyRVxBSKSgLhRgq128nP36drrM62Fmv051Htafh94I10fp_bR9fXXlIDJ_75mvgbmygJrw1RsSzdv5AXMCm_KwmYcvnenf4DjtP12tM9B0QBUdy1WETEXfCxKVlRv-tnYWG5X7qfw/index-f1-v1-a1.m3u8?sig=d_97MsU6mRjrc8S1c9kd3w&expires=1740743482",
+        "720p": "https://0uki2r61lp.guardstorage.net/hls_3/VcJ1yQrvRQTswJn-PEa4fjoSsaG8KCxs-pM4-uFYU7K53VtwMO1mKQLlu7DFkmW47JGe-ytPzDdDfXyRVxBSKSgLhRgq128nP36drrM62Fmv051Htafh94I10fp_bR9fXXlIDJ_75mvgbmygJrw1RsSzdv5AXMCm_KwmYcvnenf4DjtP12tM9B0QBUdy1WETEXfCxKVlRv-tnYWG5X7qfw/index-f2-v1-a1.m3u8?sig=d_97MsU6mRjrc8S1c9kd3w&expires=1740743482",
+        "1080p": "https://0uki2r61lp.guardstorage.net/hls_3/VcJ1yQrvRQTswJn-PEa4fjoSsaG8KCxs-pM4-uFYU7K53VtwMO1mKQLlu7DFkmW47JGe-ytPzDdDfXyRVxBSKSgLhRgq128nP36drrM62Fmv051Htafh94I10fp_bR9fXXlIDJ_75mvgbmygJrw1RsSzdv5AXMCm_KwmYcvnenf4DjtP12tM9B0QBUdy1WETEXfCxKVlRv-tnYWG5X7qfw/index-f2-v1-a1.m3u8?sig=d_97MsU6mRjrc8S1c9kd3w&expires=1740743482"
+    },
+"1126166": {
+        "title": "Amenaza En El Aire",
+        "360p": "https://otnfip7097.guardstorage.net/hls_3/gLGCywIgGqvWBzoPteijnux5MmIeb-coJ7MzvA1lbVZDxLiBa6flYFvk5T1uRo-xsr1abQykIbHtmQeh_GJnPUYZv-dPiT9VnSUsM1uWJfKmsthZqnw7uUdwfhYSH3lAgguDxOYY3-jjFMQAyeFSC16WFG3Bd1lekWN3gtOgpi02nRdYjSIWDqefLDhgCYCJlVKaLIGBc1XD3Su4mtFWXg/index-f2-v1-a1.m3u8?sig=mlGHZ_SC2WNSO8U-b88k4g&expires=1740743134",
+        "720p": "https://otnfip7097.guardstorage.net/hls_3/gLGCywIgGqvWBzoPteijnux5MmIeb-coJ7MzvA1lbVZDxLiBa6flYFvk5T1uRo-xsr1abQykIbHtmQeh_GJnPUYZv-dPiT9VnSUsM1uWJfKmsthZqnw7uUdwfhYSH3lAgguDxOYY3-jjFMQAyeFSC16WFG3Bd1lekWN3gtOgpi02nRdYjSIWDqefLDhgCYCJlVKaLIGBc1XD3Su4mtFWXg/index-f3-v1-a1.m3u8?sig=mlGHZ_SC2WNSO8U-b88k4g&expires=1740743134",
+        "1080p": "https://otnfip7097.guardstorage.net/hls_3/gLGCywIgGqvWBzoPteijnux5MmIeb-coJ7MzvA1lbVZDxLiBa6flYFvk5T1uRo-xsr1abQykIbHtmQeh_GJnPUYZv-dPiT9VnSUsM1uWJfKmsthZqnw7uUdwfhYSH3lAgguDxOYY3-jjFMQAyeFSC16WFG3Bd1lekWN3gtOgpi02nRdYjSIWDqefLDhgCYCJlVKaLIGBc1XD3Su4mtFWXg/index-f3-v1-a1.m3u8?sig=mlGHZ_SC2WNSO8U-b88k4g&expires=1740743134"
+    },
+    "762509": {
+        "title": "Mufasa El Rey León",
+        "360p": "https://zte054upd8.guardstorage.net/hls_3/zOOp8MnWQ8LLDkPqUR6nt_wq7JNc21Jd1TGs_TBV9b-Aia3tDpYc06rpQdh8SnlJ3gJ1-Gdp24wjyoTDFPPsnQ/index-f2-v1-a1.m3u8?sig=mz5wJm6pnYEum_X_-KQGvQ&expires=1740742899",
+        "720p": "https://zte054upd8.guardstorage.net/hls_3/zOOp8MnWQ8LLDkPqUR6nt_wq7JNc21Jd1TGs_TBV9b-Aia3tDpYc06rpQdh8SnlJ3gJ1-Gdp24wjyoTDFPPsnQ/index-f3-v1-a1.m3u8?sig=mz5wJm6pnYEum_X_-KQGvQ&expires=1740742899",
+        "1080p": "https://zte054upd8.guardstorage.net/hls_3/zOOp8MnWQ8LLDkPqUR6nt_wq7JNc21Jd1TGs_TBV9b-Aia3tDpYc06rpQdh8SnlJ3gJ1-Gdp24wjyoTDFPPsnQ/index-f3-v1-a1.m3u8?sig=mz5wJm6pnYEum_X_-KQGvQ&expires=1740742899"
+    },
+    "950396": {
+        "title": "El Abismo Secreto",
+        "360p": "https://hw8qpzyv9p.guardstorage.net/hls_3/qZQONg5kws3pML7ZZa3dbDEmSrpS4gRs5fUhvp8aVc6KVEZBccZDOUQ4JmErj_zYcxFOSdUz-ZbjcYyVYBktqg/index-f2-v1-a1.m3u8?sig=Kf2ANforBAJaL8JqFrLkSw&expires=1740742121",
+        "720p": "https://hw8qpzyv9p.guardstorage.net/hls_3/qZQONg5kws3pML7ZZa3dbDEmSrpS4gRs5fUhvp8aVc6KVEZBccZDOUQ4JmErj_zYcxFOSdUz-ZbjcYyVYBktqg/index-f3-v1-a1.m3u8?sig=Kf2ANforBAJaL8JqFrLkSw&expires=1740742121",
+        "1080p": "https://hw8qpzyv9p.guardstorage.net/hls_3/qZQONg5kws3pML7ZZa3dbDEmSrpS4gRs5fUhvp8aVc6KVEZBccZDOUQ4JmErj_zYcxFOSdUz-ZbjcYyVYBktqg/index-f3-v1-a1.m3u8?sig=Kf2ANforBAJaL8JqFrLkSw&expires=1740742121"
     },
     "939243": {
         "title": "Sonic 3",
-        "360p": "https://sntn3vv7pjrz.cdn-centaurus.com/hls2/01/08183/d8z9545l7k7s_,l,n,.urlset/index-f1-v1-a1.m3u8?t=DJx9j9d6TiYgOHbztvsgw1yadgBRzSqUBswhi8qIgic&s=1740213570&e=129600&f=40916559&srv=mhe7qdnw6bsc&i=0.4&sp=500&p1=mhe7qdnw6bsc&p2=mhe7qdnw6bsc&asn=264619",
-        "720p": "https://sntn3vv7pjrz.cdn-centaurus.com/hls2/01/08183/d8z9545l7k7s_,l,n,.urlset/index-f2-v1-a1.m3u8?t=DJx9j9d6TiYgOHbztvsgw1yadgBRzSqUBswhi8qIgic&s=1740213570&e=129600&f=40916559&srv=mhe7qdnw6bsc&i=0.4&sp=500&p1=mhe7qdnw6bsc&p2=mhe7qdnw6bsc&asn=264619",
-        "1080p": "https://sntn3vv7pjrz.cdn-centaurus.com/hls2/01/08183/d8z9545l7k7s_,l,n,.urlset/index-f2-v1-a1.m3u8?t=DJx9j9d6TiYgOHbztvsgw1yadgBRzSqUBswhi8qIgic&s=1740213570&e=129600&f=40916559&srv=mhe7qdnw6bsc&i=0.4&sp=500&p1=mhe7qdnw6bsc&p2=mhe7qdnw6bsc&asn=264619"
+        "360p": "https://c66afocw1c.guardstorage.net/hls_3/H1tiwIQVdhqjAqOLpDu-S9fp90S4gCAG4LMU91DDxo754RVQdgzbapOucPmi8cFGEhplZs3QWz4nCYfJx9CX2qd6zCVN__6lrYGPPALE95hKYMVb-Y2cY5wnp44E3KY7S6PGU9qpSzYR_BQP_EK5kBAJMqA0cMWtXboeM4HXP11-c6PUuqIHF1u0OtKYewT8vPkV8Y2YcH7Z1Hp0i8DtUQ/index-f1-v1-a1.m3u8?sig=iSiqON10MNr3NoKvRvi5kQ&expires=1740743808",
+        "720p": "https://c66afocw1c.guardstorage.net/hls_3/H1tiwIQVdhqjAqOLpDu-S9fp90S4gCAG4LMU91DDxo754RVQdgzbapOucPmi8cFGEhplZs3QWz4nCYfJx9CX2qd6zCVN__6lrYGPPALE95hKYMVb-Y2cY5wnp44E3KY7S6PGU9qpSzYR_BQP_EK5kBAJMqA0cMWtXboeM4HXP11-c6PUuqIHF1u0OtKYewT8vPkV8Y2YcH7Z1Hp0i8DtUQ/index-f2-v1-a1.m3u8?sig=iSiqON10MNr3NoKvRvi5kQ&expires=1740743808",
+        "1080p": "https://c66afocw1c.guardstorage.net/hls_3/H1tiwIQVdhqjAqOLpDu-S9fp90S4gCAG4LMU91DDxo754RVQdgzbapOucPmi8cFGEhplZs3QWz4nCYfJx9CX2qd6zCVN__6lrYGPPALE95hKYMVb-Y2cY5wnp44E3KY7S6PGU9qpSzYR_BQP_EK5kBAJMqA0cMWtXboeM4HXP11-c6PUuqIHF1u0OtKYewT8vPkV8Y2YcH7Z1Hp0i8DtUQ/index-f2-v1-a1.m3u8?sig=iSiqON10MNr3NoKvRvi5kQ&expires=1740743808"
     }, 
     "710295": {
         "title": "Hombre Lobo",

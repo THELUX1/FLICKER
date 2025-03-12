7509756753a8e3f3294065e25cc85118d378c433
THELUX1 <122177610+THELUX1@users.noreply.github.com>
2025-03-11 23:17:42 -0300

    Add files via upload

--- videos.js
+++ videos.js
@@ -2,27 +2,27 @@
 const movieVideos = {
     "1272149": {
         "title": "Bridget Jones: Loca Por Él",
-        "360p": "https://wpvsahsvl0yo.acek-cdn.com/hls2/01/03824/g2l7o2r2z5kn_n/index-v1-a1.m3u8?t=a5MQroQafz7RTSRZXxIr00BikEIvnrLvKI1JeNYSviM&s=1741327793&e=129600&f=19585118&srv=yCdcGNsOcuCU&i=0.4&sp=500&p1=yCdcGNsOcuCU&p2=yCdcGNsOcuCU&asn=264619",
-        "720p": "https://wpvsahsvl0yo.acek-cdn.com/hls2/01/03824/g2l7o2r2z5kn_n/index-v1-a1.m3u8?t=a5MQroQafz7RTSRZXxIr00BikEIvnrLvKI1JeNYSviM&s=1741327793&e=129600&f=19585118&srv=yCdcGNsOcuCU&i=0.4&sp=500&p1=yCdcGNsOcuCU&p2=yCdcGNsOcuCU&asn=264619",
-        "1080p": "https://wpvsahsvl0yo.acek-cdn.com/hls2/01/03824/g2l7o2r2z5kn_n/index-v1-a1.m3u8?t=a5MQroQafz7RTSRZXxIr00BikEIvnrLvKI1JeNYSviM&s=1741327793&e=129600&f=19585118&srv=yCdcGNsOcuCU&i=0.4&sp=500&p1=yCdcGNsOcuCU&p2=yCdcGNsOcuCU&asn=264619"
-    },
+        "360p": "https://v2.fiestareel.com/mov/2025/1272149-156564/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2025/1272149-156564/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2025/1272149-156564/720/720p.m3u8"
+    },        
     "933260": {
         "title": "La sustancia",
-        "360p": "https://xyve3dzn2w.guardstorage.net/hls_3/7pvQIdsxe6FabtJ2nwp5Ou_fnvf2zyBlv-EAkd1AdqKDVg3KutMW5bUKACBU6TOBffNnDAcmwbiQmlnyrCfWQuXLMdw8oXe9b6c-9qcm9dxen3g9lVg3L829S98JfuxrDrDyl1jLssUpb5zIWAzxd3mV17WOMypZ7dFcHhXlEWBDolJuPTsF-DsvlTpBLDHr6Gdma3JnkJYi8LuJzV5ofA/index-f2-v1-a1.m3u8?sig=fyxlCU31aBHUd2CWgLZDig&expires=1741169323",
-        "720p": "https://xyve3dzn2w.guardstorage.net/hls_3/7pvQIdsxe6FabtJ2nwp5Ou_fnvf2zyBlv-EAkd1AdqKDVg3KutMW5bUKACBU6TOBffNnDAcmwbiQmlnyrCfWQuXLMdw8oXe9b6c-9qcm9dxen3g9lVg3L829S98JfuxrDrDyl1jLssUpb5zIWAzxd3mV17WOMypZ7dFcHhXlEWBDolJuPTsF-DsvlTpBLDHr6Gdma3JnkJYi8LuJzV5ofA/index-f2-v1-a1.m3u8?sig=fyxlCU31aBHUd2CWgLZDig&expires=1741169323",
+        "360p": "https://v2.fiestareel.com/mov/2024/933260-156010/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2024/933260-156010/720/720p.m3u8",
         "1080p": "https://xyve3dzn2w.guardstorage.net/hls_3/7pvQIdsxe6FabtJ2nwp5Ou_fnvf2zyBlv-EAkd1AdqKDVg3KutMW5bUKACBU6TOBffNnDAcmwbiQmlnyrCfWQuXLMdw8oXe9b6c-9qcm9dxen3g9lVg3L829S98JfuxrDrDyl1jLssUpb5zIWAzxd3mV17WOMypZ7dFcHhXlEWBDolJuPTsF-DsvlTpBLDHr6Gdma3JnkJYi8LuJzV5ofA/index-f3-v1-a1.m3u8?sig=fyxlCU31aBHUd2CWgLZDig&expires=1741169323"
     },
     "823219": {
         "title": "Flow",
-        "360p": "https://akm-cdn-play-web.onfilom.com/70d62c448d1a71efbfc987c7371d0102/de9e99578e8448dc96428924ffd150dd-4acea4010fa084e84e5f3aef9d32811f-fd.m3u8?hdnts=exp=1741430378_acl=/70d62c448d1a71efbfc987c7371d0102/*_hmac=84827932fe5db218d42e00325677698eaae68353ee1ab74dd7914236f0294d9c",
-        "720p": "https://akm-cdn-play-web.onfilom.com/70d62c448d1a71efbfc987c7371d0102/de9e99578e8448dc96428924ffd150dd-2a36e0a3d8613822e53341ad74d37310-ld.m3u8?hdnts=exp=1741430378_acl=/70d62c448d1a71efbfc987c7371d0102/*_hmac=84827932fe5db218d42e00325677698eaae68353ee1ab74dd7914236f0294d9c",
-        "1080p": "https://akm-cdn-play-web.onfilom.com/70d62c448d1a71efbfc987c7371d0102/de9e99578e8448dc96428924ffd150dd-31f43b7a1cd6b8c49b9e46905d4d8866-sd.m3u8?hdnts=exp=1741430378_acl=/70d62c448d1a71efbfc987c7371d0102/*_hmac=84827932fe5db218d42e00325677698eaae68353ee1ab74dd7914236f0294d9c"
+        "360p": "https://akm-cdn-play-web.onfilom.com/70d62c448d1a71efbfc987c7371d0102/de9e99578e8448dc96428924ffd150dd-31f43b7a1cd6b8c49b9e46905d4d8866-sd.m3u8?hdnts=exp=1741754593_acl=/70d62c448d1a71efbfc987c7371d0102/*_hmac=72c15b6a768e408e7539ed2e49a40e8cf9774dcbdf369dea6659d7508b0ca62a",
+        "720p": "https://akm-cdn-play-web.onfilom.com/70d62c448d1a71efbfc987c7371d0102/de9e99578e8448dc96428924ffd150dd-31f43b7a1cd6b8c49b9e46905d4d8866-sd.m3u8?hdnts=exp=1741754593_acl=/70d62c448d1a71efbfc987c7371d0102/*_hmac=72c15b6a768e408e7539ed2e49a40e8cf9774dcbdf369dea6659d7508b0ca62a",
+        "1080p": "https://akm-cdn-play-web.onfilom.com/70d62c448d1a71efbfc987c7371d0102/de9e99578e8448dc96428924ffd150dd-31f43b7a1cd6b8c49b9e46905d4d8866-sd.m3u8?hdnts=exp=1741754593_acl=/70d62c448d1a71efbfc987c7371d0102/*_hmac=72c15b6a768e408e7539ed2e49a40e8cf9774dcbdf369dea6659d7508b0ca62a"
     },
     "1064213": {
         "title": "Anora",
-        "360p": "https://akm-cdn-play-web.swplayer.com/00747ad3f25e71efbfef4177d0d10102/de98d363b5df4f8ba6d48dea02c95876-834b9be3e48bcc72b9a3a11e5771ccab-fd.m3u8?hdnts=exp=1741386768_acl=/00747ad3f25e71efbfef4177d0d10102/*_hmac=d94fea309c7d479269bae41639776579a56c921da7ea13b0436835fa2650b25b",
-        "720p": "https://akm-cdn-play-web.swplayer.com/00747ad3f25e71efbfef4177d0d10102/de98d363b5df4f8ba6d48dea02c95876-834b9be3e48bcc72b9a3a11e5771ccab-fd.m3u8?hdnts=exp=1741386768_acl=/00747ad3f25e71efbfef4177d0d10102/*_hmac=d94fea309c7d479269bae41639776579a56c921da7ea13b0436835fa2650b25b",
-        "1080p": "https://akm-cdn-play-web.swplayer.com/00747ad3f25e71efbfef4177d0d10102/de98d363b5df4f8ba6d48dea02c95876-01a99083a931dd1a7c4347eb9deee5ce-sd.m3u8?hdnts=exp=1741386768_acl=/00747ad3f25e71efbfef4177d0d10102/*_hmac=d94fea309c7d479269bae41639776579a56c921da7ea13b0436835fa2650b25b"
+        "360p": "https://v2.fiestareel.com/mov/2024/1064213-156537/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2024/1064213-156537/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2024/1064213-156537/720/720p.m3u8"
     },
     "549509": {
         "title": "El brutalista",
@@ -32,9 +32,9 @@ const movieVideos = {
     },
     "974576": {
         "title": "Cónclave",
-        "360p": "https://akm-cdn-play-web.swplayer.com/70d918b6bd0671ef808b86c6360c0102/a6a02a6ec4f3440b8a749d4b2957a6ca-744f487d0d91c3d8b88ba5912beb6dd9-fd.m3u8?hdnts=exp=1741503194_acl=/70d918b6bd0671ef808b86c6360c0102/*_hmac=155e1bfc052ff89bb92be67da37a11d4bbae1e917255ba1eda2931ebd3f7d19c",
-        "720p": "https://akm-cdn-play-web.swplayer.com/70d918b6bd0671ef808b86c6360c0102/a6a02a6ec4f3440b8a749d4b2957a6ca-a3aa7e4a010671da7cc2795250166fd7-ld.m3u8?hdnts=exp=1741503194_acl=/70d918b6bd0671ef808b86c6360c0102/*_hmac=155e1bfc052ff89bb92be67da37a11d4bbae1e917255ba1eda2931ebd3f7d19c",
-        "1080p": "https://akm-cdn-play-web.swplayer.com/70d918b6bd0671ef808b86c6360c0102/a6a02a6ec4f3440b8a749d4b2957a6ca-9d1e8e3c531b4571367538cf9c654268-sd.m3u8?hdnts=exp=1741503194_acl=/70d918b6bd0671ef808b86c6360c0102/*_hmac=155e1bfc052ff89bb92be67da37a11d4bbae1e917255ba1eda2931ebd3f7d19c"
+        "360p": "https://v2.fiestareel.com/mov/2024/974576-156443/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2024/974576-156443/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2024/974576-156443/720/720p.m3u8"
     },
     "1035048": {
         "title": "Criaturas: Línea de extinción",
@@ -86,9 +86,9 @@ const movieVideos = {
     },
 "1294203": {
         "title": "Culpa Mía: Londres",
-        "360p": "https://jdou5wo6ffta.acek-cdn.com/hls2/01/03824/l9uzuwndv53m_n/index-v1-a1.m3u8?t=5GMrSRCcJZ_Taml8txD_qbZ8JHQ_ztRHmh75fogsOkc&s=1741417408&e=129600&f=19123026&srv=rtlcORIeSmi8&i=0.4&sp=500&p1=rtlcORIeSmi8&p2=rtlcORIeSmi8&asn=264619",
-        "720p": "https://jdou5wo6ffta.acek-cdn.com/hls2/01/03824/l9uzuwndv53m_n/index-v1-a1.m3u8?t=5GMrSRCcJZ_Taml8txD_qbZ8JHQ_ztRHmh75fogsOkc&s=1741417408&e=129600&f=19123026&srv=rtlcORIeSmi8&i=0.4&sp=500&p1=rtlcORIeSmi8&p2=rtlcORIeSmi8&asn=264619",
-        "1080p": "https://jdou5wo6ffta.acek-cdn.com/hls2/01/03824/l9uzuwndv53m_n/index-v1-a1.m3u8?t=5GMrSRCcJZ_Taml8txD_qbZ8JHQ_ztRHmh75fogsOkc&s=1741417408&e=129600&f=19123026&srv=rtlcORIeSmi8&i=0.4&sp=500&p1=rtlcORIeSmi8&p2=rtlcORIeSmi8&asn=264619"
+        "360p": "https://v2.fiestareel.com/mov/2025/1294203-156644/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2025/1294203-156644/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2025/1294203-156644/720/720p.m3u8"
     },
 "539972": {
         "title": "Kraven el cazador",
@@ -104,45 +104,45 @@ const movieVideos = {
     },
 "822119": {
         "title": "Capitán América: Un nuevo mundo",
-        "360p": "https://7f12638xuzfg.premilkyway.com/hls2/01/08546/uagcpcfsl4la_,l,n,h,.urlset/index-f1-v1-a1.m3u8?t=yzNoYn-kb4EAk0xvQUdd2T6llGv5nqx_yAuDLhwl5lc&s=1741401339&e=129600&f=42732441&srv=jubDiYuWLQCqfHI&i=0.4&sp=500&p1=jubDiYuWLQCqfHI&p2=jubDiYuWLQCqfHI&asn=264619",
-        "720p": "https://7f12638xuzfg.premilkyway.com/hls2/01/08546/uagcpcfsl4la_,l,n,h,.urlset/index-f2-v1-a1.m3u8?t=yzNoYn-kb4EAk0xvQUdd2T6llGv5nqx_yAuDLhwl5lc&s=1741401339&e=129600&f=42732441&srv=jubDiYuWLQCqfHI&i=0.4&sp=500&p1=jubDiYuWLQCqfHI&p2=jubDiYuWLQCqfHI&asn=264619",
-        "1080p": "https://7f12638xuzfg.premilkyway.com/hls2/01/08546/uagcpcfsl4la_,l,n,h,.urlset/index-f3-v1-a1.m3u8?t=yzNoYn-kb4EAk0xvQUdd2T6llGv5nqx_yAuDLhwl5lc&s=1741401339&e=129600&f=42732441&srv=jubDiYuWLQCqfHI&i=0.4&sp=500&p1=jubDiYuWLQCqfHI&p2=jubDiYuWLQCqfHI&asn=264619"
+        "360p": "https://v2.fiestareel.com/mov/2025/822119-156597/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2025/822119-156597/480/480p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2025/822119-156597/720/720p.m3u8"
     },
 "1084199": {
         "title": "Compañera Perfecta",
-        "360p": "https://hi3thh5oxxww.acek-cdn.com/hls2/01/03849/90822h0x0x2w_n/index-v1-a1.m3u8?t=ZrTRs9wI5asC33ISKZyjKqjP3ms19C1DqaKH75J6G8M&s=1741401827&e=129600&f=19251329&srv=LFDu7HStkKAt&i=0.4&sp=500&p1=LFDu7HStkKAt&p2=LFDu7HStkKAt&asn=264619",
-        "720p": "https://hi3thh5oxxww.acek-cdn.com/hls2/01/03849/90822h0x0x2w_n/index-v1-a1.m3u8?t=ZrTRs9wI5asC33ISKZyjKqjP3ms19C1DqaKH75J6G8M&s=1741401827&e=129600&f=19251329&srv=LFDu7HStkKAt&i=0.4&sp=500&p1=LFDu7HStkKAt&p2=LFDu7HStkKAt&asn=264619",
-        "1080p": "https://hi3thh5oxxww.acek-cdn.com/hls2/01/03849/90822h0x0x2w_n/index-v1-a1.m3u8?t=ZrTRs9wI5asC33ISKZyjKqjP3ms19C1DqaKH75J6G8M&s=1741401827&e=129600&f=19251329&srv=LFDu7HStkKAt&i=0.4&sp=500&p1=LFDu7HStkKAt&p2=LFDu7HStkKAt&asn=264619"
+        "360p": "https://v2.fiestareel.com/mov/2025/1084199-156685/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2025/1084199-156685/720/720p.m3u8",
+        "1080p": ""
     },
 "1241982": {
         "title": "Moana 2",
-        "360p": "https://akm-cdn-play-web.swplayer.com/40a19674adfb71efbfbf97c6360c0102/a206070514bb4a0fbe333c704ea63103-6399bba9ce8e29ae4b2fef2986534eed-fd.m3u8?hdnts=exp=1741387134_acl=/40a19674adfb71efbfbf97c6360c0102/*_hmac=18b5cd4248cec7dc7da5be9a95c45f4ba75987559ad65c285b8d9d1421f854fe",
-        "720p": "https://akm-cdn-play-web.swplayer.com/40a19674adfb71efbfbf97c6360c0102/a206070514bb4a0fbe333c704ea63103-642f8931f022d00a9fd830bb158f7505-sd.m3u8?hdnts=exp=1741387134_acl=/40a19674adfb71efbfbf97c6360c0102/*_hmac=18b5cd4248cec7dc7da5be9a95c45f4ba75987559ad65c285b8d9d1421f854fe",
-        "1080p": "https://akm-cdn-play-web.swplayer.com/40a19674adfb71efbfbf97c6360c0102/a206070514bb4a0fbe333c704ea63103-1d24a2853a26444bdcc9a53e7bd10806-hd.m3u8?hdnts=exp=1741387134_acl=/40a19674adfb71efbfbf97c6360c0102/*_hmac=18b5cd4248cec7dc7da5be9a95c45f4ba75987559ad65c285b8d9d1421f854fe"
+        "360p": "https://v2.fiestareel.com/mov/2024/1241982-156362/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2024/1241982-156362/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2024/1241982-156362/720/720p.m3u8"
     },
 "1126166": {
         "title": "Amenaza En El Aire",
-        "360p": "https://otnfip7097.guardstorage.net/hls_3/gLGCywIgGqvWBzoPteijnux5MmIeb-coJ7MzvA1lbVZDxLiBa6flYFvk5T1uRo-xsr1abQykIbHtmQeh_GJnPUYZv-dPiT9VnSUsM1uWJfKmsthZqnw7uUdwfhYSH3lAgguDxOYY3-jjFMQAyeFSC16WFG3Bd1lekWN3gtOgpi02nRdYjSIWDqefLDhgCYCJlVKaLIGBc1XD3Su4mtFWXg/index-f2-v1-a1.m3u8?sig=mlGHZ_SC2WNSO8U-b88k4g&expires=1740743134",
-        "720p": "https://otnfip7097.guardstorage.net/hls_3/gLGCywIgGqvWBzoPteijnux5MmIeb-coJ7MzvA1lbVZDxLiBa6flYFvk5T1uRo-xsr1abQykIbHtmQeh_GJnPUYZv-dPiT9VnSUsM1uWJfKmsthZqnw7uUdwfhYSH3lAgguDxOYY3-jjFMQAyeFSC16WFG3Bd1lekWN3gtOgpi02nRdYjSIWDqefLDhgCYCJlVKaLIGBc1XD3Su4mtFWXg/index-f3-v1-a1.m3u8?sig=mlGHZ_SC2WNSO8U-b88k4g&expires=1740743134",
-        "1080p": "https://otnfip7097.guardstorage.net/hls_3/gLGCywIgGqvWBzoPteijnux5MmIeb-coJ7MzvA1lbVZDxLiBa6flYFvk5T1uRo-xsr1abQykIbHtmQeh_GJnPUYZv-dPiT9VnSUsM1uWJfKmsthZqnw7uUdwfhYSH3lAgguDxOYY3-jjFMQAyeFSC16WFG3Bd1lekWN3gtOgpi02nRdYjSIWDqefLDhgCYCJlVKaLIGBc1XD3Su4mtFWXg/index-f3-v1-a1.m3u8?sig=mlGHZ_SC2WNSO8U-b88k4g&expires=1740743134"
+        "360p": "https://v2.fiestareel.com/mov/2025/1126166-156540/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2025/1126166-156540/720/720p.m3u8",
+        "1080p": ""
     },
     "762509": {
         "title": "Mufasa El Rey León",
-        "360p": "https://akm-cdn-play-web.swplayer.com/a0236babcbd671ef814a86c6360c0102/3e67dbe1512d4dc893a54560768b20fa-88132d0ee80f1a11b2cf848108927980-fd.m3u8?hdnts=exp=1741489470_acl=/a0236babcbd671ef814a86c6360c0102/*_hmac=bbd24f5dc573b85ccb4259de17fc4d30672645bcbfa9ca0340f8d96fc00532a0",
-        "720p": "https://akm-cdn-play-web.swplayer.com/a0236babcbd671ef814a86c6360c0102/3e67dbe1512d4dc893a54560768b20fa-a63109e1f44c74225e2248b7f3fd1570-ld.m3u8?hdnts=exp=1741489470_acl=/a0236babcbd671ef814a86c6360c0102/*_hmac=bbd24f5dc573b85ccb4259de17fc4d30672645bcbfa9ca0340f8d96fc00532a0",
-        "1080p": "https://akm-cdn-play-web.swplayer.com/a0236babcbd671ef814a86c6360c0102/3e67dbe1512d4dc893a54560768b20fa-c107b7aa5aa0ec93ac25bd0d1a9a9d5d-sd.m3u8?hdnts=exp=1741489470_acl=/a0236babcbd671ef814a86c6360c0102/*_hmac=bbd24f5dc573b85ccb4259de17fc4d30672645bcbfa9ca0340f8d96fc00532a0"
+        "360p": "https://v2.fiestareel.com/mov/2024/762509-156193/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2024/762509-156193/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2024/762509-156193/720/720p.m3u8"
     },
     "950396": {
         "title": "El Abismo Secreto",
-        "360p": "https://akm-cdn-play-web.swplayer.com/c0928b6ef70671ef86334177d1c00102/2230c9ab671e42abb79cf9c93890b3e6-631609d1c96636aa105e0b95e5182c6d-fd.m3u8?hdnts=exp=1741488636_acl=/c0928b6ef70671ef86334177d1c00102/*_hmac=88eb4007768e8f557c7191822d1cf562b15895f5170441c2bea86db9b7ef6b2e",
-        "720p": "https://akm-cdn-play-web.swplayer.com/c0928b6ef70671ef86334177d1c00102/2230c9ab671e42abb79cf9c93890b3e6-102e26cf6d0a02d4fb7bd87c20571d99-sd.m3u8?hdnts=exp=1741488636_acl=/c0928b6ef70671ef86334177d1c00102/*_hmac=88eb4007768e8f557c7191822d1cf562b15895f5170441c2bea86db9b7ef6b2e",
-        "1080p": "https://akm-cdn-play-web.swplayer.com/c0928b6ef70671ef86334177d1c00102/2230c9ab671e42abb79cf9c93890b3e6-bb995a26a2e0a2efb5b2a32735e8bf15-hd.m3u8?hdnts=exp=1741488636_acl=/c0928b6ef70671ef86334177d1c00102/*_hmac=88eb4007768e8f557c7191822d1cf562b15895f5170441c2bea86db9b7ef6b2e"
+        "360p": "https://v2.fiestareel.com/mov/2025/950396-156617/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2025/950396-156617/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2025/950396-156617/720/720p.m3u8"
     },
     "939243": {
         "title": "Sonic 3",
-        "360p": "https://c66afocw1c.guardstorage.net/hls_3/H1tiwIQVdhqjAqOLpDu-S9fp90S4gCAG4LMU91DDxo754RVQdgzbapOucPmi8cFGEhplZs3QWz4nCYfJx9CX2qd6zCVN__6lrYGPPALE95hKYMVb-Y2cY5wnp44E3KY7S6PGU9qpSzYR_BQP_EK5kBAJMqA0cMWtXboeM4HXP11-c6PUuqIHF1u0OtKYewT8vPkV8Y2YcH7Z1Hp0i8DtUQ/index-f1-v1-a1.m3u8?sig=iSiqON10MNr3NoKvRvi5kQ&expires=1740743808",
-        "720p": "https://c66afocw1c.guardstorage.net/hls_3/H1tiwIQVdhqjAqOLpDu-S9fp90S4gCAG4LMU91DDxo754RVQdgzbapOucPmi8cFGEhplZs3QWz4nCYfJx9CX2qd6zCVN__6lrYGPPALE95hKYMVb-Y2cY5wnp44E3KY7S6PGU9qpSzYR_BQP_EK5kBAJMqA0cMWtXboeM4HXP11-c6PUuqIHF1u0OtKYewT8vPkV8Y2YcH7Z1Hp0i8DtUQ/index-f2-v1-a1.m3u8?sig=iSiqON10MNr3NoKvRvi5kQ&expires=1740743808",
-        "1080p": "https://c66afocw1c.guardstorage.net/hls_3/H1tiwIQVdhqjAqOLpDu-S9fp90S4gCAG4LMU91DDxo754RVQdgzbapOucPmi8cFGEhplZs3QWz4nCYfJx9CX2qd6zCVN__6lrYGPPALE95hKYMVb-Y2cY5wnp44E3KY7S6PGU9qpSzYR_BQP_EK5kBAJMqA0cMWtXboeM4HXP11-c6PUuqIHF1u0OtKYewT8vPkV8Y2YcH7Z1Hp0i8DtUQ/index-f2-v1-a1.m3u8?sig=iSiqON10MNr3NoKvRvi5kQ&expires=1740743808"
+        "360p": "https://v2.fiestareel.com/mov/2024/939243-156549/480/480p.m3u8",
+        "720p": "https://v2.fiestareel.com/mov/2024/939243-156549/720/720p.m3u8",
+        "1080p": "https://v2.fiestareel.com/mov/2024/939243-156549/720/720p.m3u8"
     }, 
     "710295": {
         "title": "Hombre Lobo",
